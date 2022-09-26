4e0b1f1885b57c8615d8616baef35cc8fd64fc73
unknown <fabianavilesfaar@gmail.com>
2022-09-26 15:51:25 -0600

    Railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
+// use Illuminate\Support\Facades\Schema;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
         if($this->app->environment('production'))
         {
             URL::forceScheme('https');
-            Schema::defaultStringLength(191);
+            // Schema::defaultStringLength(191);
         }
     }
 }
