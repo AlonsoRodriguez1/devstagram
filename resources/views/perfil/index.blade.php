e9cba0a024b34054cfe87b7cbc3674d98010b044
unknown <fabianavilesfaar@gmail.com>
2022-09-26 16:16:01 -0600

    Heroku

--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -22,6 +22,7 @@
                     <label class="mb-2 block uppercase text-gray-500 font-bold" for="imagen">Imagen Perfil</label>
                     <input type="file" id="imagen" name="imagen" class="border p-3 w-full rounded-lg value="" accept=".jpg, .jpeg, .png">
                 </div>
+                {{echo "j"}}
 
                 <input type="submit" value="Guardar Cambios" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
             </form>
