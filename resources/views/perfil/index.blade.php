e52dc6e7d6e8991fbd5ec0a46695483658bde5fe
unknown <fabianavilesfaar@gmail.com>
2022-09-26 17:03:34 -0600

    Railway

--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -17,12 +17,11 @@
                         <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
                     @enderror
                 </div>
-                {{echo "r" }}
+                
                 <div class="mb-5">
                     <label class="mb-2 block uppercase text-gray-500 font-bold" for="imagen">Imagen Perfil</label>
                     <input type="file" id="imagen" name="imagen" class="border p-3 w-full rounded-lg value="" accept=".jpg, .jpeg, .png">
                 </div>
-                {{echo "j"}}
 
                 <input type="submit" value="Guardar Cambios" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
             </form>
